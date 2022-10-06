948181049e938bd0213a887a5b399c66c13de315
Tung Nguyen <tungtn3@gmail.com>
2022-10-06 21:04:50 +1100

    update disassembleInstruction for run-length encoded line data

--- src/debug.c
+++ src/debug.c
@@ -39,10 +39,10 @@ static int simpleInstruction(const char* name, int offset) {
 int disassembleInstruction(Chunk* chunk, int offset) {
   printf("%04d ", offset);
   if (offset > 0 &&
-      chunk->lines[offset] == chunk->lines[offset - 1]) {
+      getLine(chunk, offset) == getLine(chunk, offset - 1)) {
     printf("   | ");
   } else {
-    printf("%4d ", chunk->lines[offset]);
+    printf("%4d ", getLine(chunk, offset));
   }
 
   uint8_t instruction = chunk->code[offset];
