ac69b362724db3e88cb9c2cb28231a9e0ae4f5d6
Tung Nguyen <tungtn3@gmail.com>
2022-10-18 16:55:56 +1100

    pack file ptr with membuf in debug_test.c, wrap long param and arg lists
    The DisassembleChunk fixture in debug_test.c now fcloses its memstreams,
    so the tests only have to worry about fflushing before reading MemBuf
    contents.
    
    Also switched to bin packing of long parameter and argument lists to
    prevent excessive vertical scrolling.

--- src/debug_test.c
+++ src/debug_test.c
@@ -1,5 +1,7 @@
 #include "debug.h"
 
+#include <stdio.h>
+
 #include "utest.h"
 
 #include "chunk.h"
@@ -9,25 +11,26 @@
 typedef struct {
   char* buf;
   size_t size;
+  FILE* fptr;
 } MemBuf;
 
 struct DisassembleChunk {
   Chunk chunk;
   MemBuf out;
   MemBuf err;
-  FILE* fout;
-  FILE* ferr;
 };
 
 UTEST_F_SETUP(DisassembleChunk) {
-  ufx->fout = open_memstream(&ufx->out.buf, &ufx->out.size);
-  ufx->ferr = open_memstream(&ufx->err.buf, &ufx->err.size);
   initChunk(&ufx->chunk);
+  ufx->out.fptr = open_memstream(&ufx->out.buf, &ufx->out.size);
+  ufx->err.fptr = open_memstream(&ufx->err.buf, &ufx->err.size);
   ASSERT_TRUE(1);
 }
 
 UTEST_F_TEARDOWN(DisassembleChunk) {
   freeChunk(&ufx->chunk);
+  fclose(ufx->out.fptr);
+  fclose(ufx->err.fptr);
   free(ufx->out.buf);
   free(ufx->err.buf);
   ASSERT_TRUE(1);
@@ -35,20 +38,21 @@ UTEST_F_TEARDOWN(DisassembleChunk) {
 
 UTEST_F(DisassembleChunk, UnknownOpCode) {
   writeChunk(&ufx->chunk, 255, 1);
-  disassembleChunk(ufx->fout, ufx->ferr, &ufx->chunk, "");
-  fclose(ufx->fout);
-  fclose(ufx->ferr);
+  disassembleChunk(ufx->out.fptr, ufx->err.fptr, &ufx->chunk, "");
 
+  fflush(ufx->out.fptr);
+  fflush(ufx->err.fptr);
   ASSERT_LT(0ul, ufx->err.size);
   EXPECT_STREQ("Unknown opcode 255\n", ufx->err.buf);
 }
 
 UTEST_F(DisassembleChunk, Chapter14Sample1) {
   writeChunk(&ufx->chunk, OP_RETURN, 123);
-  disassembleChunk(ufx->fout, ufx->ferr, &ufx->chunk, "test chunk");
-  fclose(ufx->fout);
-  fclose(ufx->ferr);
+  disassembleChunk(
+      ufx->out.fptr, ufx->err.fptr, &ufx->chunk, "test chunk");
 
+  fflush(ufx->out.fptr);
+  fflush(ufx->err.fptr);
   ASSERT_LT(0ul, ufx->out.size);
   const char outMsg[] =
       "== test chunk ==\n"
@@ -62,10 +66,11 @@ UTEST_F(DisassembleChunk, Chapter14Sample2) {
   writeChunk(&ufx->chunk, OP_CONSTANT, 123);
   writeChunk(&ufx->chunk, constant, 123);
   writeChunk(&ufx->chunk, OP_RETURN, 123);
-  disassembleChunk(ufx->fout, ufx->ferr, &ufx->chunk, "test chunk");
-  fclose(ufx->fout);
-  fclose(ufx->ferr);
+  disassembleChunk(
+      ufx->out.fptr, ufx->err.fptr, &ufx->chunk, "test chunk");
 
+  fflush(ufx->out.fptr);
+  fflush(ufx->err.fptr);
   ASSERT_LT(0ul, ufx->out.size);
   const char outMsg[] =
       "== test chunk ==\n"
--- src/memory.h
+++ src/memory.h
@@ -7,9 +7,8 @@
 #define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity)*2)
 
 #define GROW_ARRAY(type, pointer, oldCount, newCount) \
-  (type*)reallocate(pointer, \
-      sizeof(type) * (oldCount), \
-      sizeof(type) * (newCount))
+  (type*)reallocate( \
+      pointer, sizeof(type) * (oldCount), sizeof(type) * (newCount))
 
 #define FREE_ARRAY(type, pointer, oldCount) \
   reallocate(pointer, sizeof(type) * (oldCount), 0)
