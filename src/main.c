4727235537de50e70c94d7fdae400e7dd23334f0
Tung Nguyen <tungtn3@gmail.com>
2022-12-01 15:33:25 +1100

    allow files of unknown length to be read
    This allows things like `./clox <(echo 'print "hi";')` to work instead
    of failing like before.

--- src/main.c
+++ src/main.c
@@ -60,41 +60,39 @@ static void repl(void) {
   }
 }
 
-static char* readFile(const char* path) {
+static void readFile(MemBuf* mbuf, const char* path) {
   FILE* file = fopen(path, "rb");
   if (file == NULL) {
     fprintf(stderr, "Could not open file \"%s\".\n", path);
     exit(74);
   }
 
-  fseek(file, 0L, SEEK_END);
-  size_t fileSize = ftell(file);
-  rewind(file);
+  char tmp[1024];
+  size_t num;
+  while ((num = fread(&tmp, 1, sizeof(tmp), file)) > 0) {
+    fwrite(&tmp, 1, num, mbuf->fptr);
+  }
+  fflush(mbuf->fptr);
 
-  char* buffer = (char*)malloc(fileSize + 1);
-  if (buffer == NULL) {
-    fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+  if (ferror(file)) {
+    perror("readFile");
+    fclose(file);
     exit(74);
   }
-
-  size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
-  if (bytesRead < fileSize) {
-    fprintf(stderr, "Could not read file \"%s\".\n", path);
-    exit(74);
-  }
-
-  buffer[bytesRead] = '\0';
 
   fclose(file);
-  return buffer;
 }
 
 static void runFile(const char* path) {
   VM vm;
+  MemBuf source;
+  InterpretResult result;
+
   initVM(&vm, stdout, stderr);
-  char* source = readFile(path);
-  InterpretResult result = interpret(&vm, source);
-  free(source);
+  initMemBuf(&source);
+  readFile(&source, path);
+  result = interpret(&vm, source.buf);
+  freeMemBuf(&source);
   freeVM(&vm);
 
   if (result == INTERPRET_COMPILE_ERROR) {
